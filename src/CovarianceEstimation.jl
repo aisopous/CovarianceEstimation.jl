13e3c29e445c21035fd1c1d18d8811380ee1add1
Anton Isopoussu <anton.isopoussu@invenialabs.co.uk>
2021-03-16 15:17:53 -0500

    Add basic Wasserstein shrinkage

--- src/CovarianceEstimation.jl
+++ src/CovarianceEstimation.jl
@@ -6,17 +6,19 @@ using LinearAlgebra
 import StatsBase: cov
 
 export cov
+export prec
 export CovarianceEstimator, SimpleCovariance,
     LinearShrinkage,
     # Targets for linear shrinkage
     DiagonalUnitVariance, DiagonalCommonVariance, DiagonalUnequalVariance,
     CommonCovariance, PerfectPositiveCorrelation, ConstantCorrelation,
     # Eigendecomposition-based methods
-    AnalyticalNonlinearShrinkage
-
-
+    AnalyticalNonlinearShrinkage,
+    WassersteinShrinkage
+    
 include("utils.jl")
 include("linearshrinkage.jl")
 include("nonlinearshrinkage.jl")
+include("robust.jl")
 
 end # module
